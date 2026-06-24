38b01921c91287cdf9c383346ee04bc54c7258ac
Adewale <adewale.oladiti28@gmail.com>
2026-06-24 20:53:04 +0100

    updated admin.py to register expenses and category

--- DjangoProject/expenseflow/expenses/models.py
+++ DjangoProject/expenseflow/expenses/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(
+        max_length=100
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+
+class Expense(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(
+        max_length=200
+    )
+
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    description = models.TextField(
+        blank=True
+    )
+
+    date = models.DateField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title
+
